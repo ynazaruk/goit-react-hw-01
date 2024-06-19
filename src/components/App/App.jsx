50b218cfbeaf44dee36668caed0ab832db956b51
ynazaruk <nazaruk.ya@gmail.com>
2024-06-20 01:17:50 +0300

    task-1-all

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,16 +1,16 @@
 import userData from "../../userData.json";
-import { Profile } from "../Profile/Profile";
+import Profile from "../Profile/Profile";
 
-export const App = () => {
+export default function App() {
   return (
     <>
       <Profile
         name={userData.username}
         tag={userData.tag}
         location={userData.location}
-        image={userData.image}
+        image={userData.avatar}
         stats={userData.stats}
       />
     </>
   );
-};
+}
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,30 +1,29 @@
-import userData from "../../userData.json";
 import css from "../Profile/Profile.module.css";
 
-export const Profile = () => {
+export default function Profile({ username, tag, location, image, stats }) {
   return (
     <div className={css.container}>
       <div>
-        <img className={css.image} src={userData.avatar} alt="User avatar" />
-        <p className={css.name}>{userData.username}</p>
-        <p className={css.text}>@{userData.tag}</p>
-        <p className={css.text}>{userData.location}</p>
+        <img className={css.image} src={image} alt="User avatar" />
+        <p className={css.name}>{username}</p>
+        <p className={css.text}>@{tag}</p>
+        <p className={css.text}>{location}</p>
       </div>
 
       <ul className={css.list}>
         <li className={css.listItem}>
           <span className={css.info}>Followers</span>
-          <span className={css.quantity}>{userData.stats.followers}</span>
+          <span className={css.quantity}>{stats.followers}</span>
         </li>
         <li className={css.listItem}>
           <span className={css.info}>Views</span>
-          <span className={css.quantity}>{userData.stats.views}</span>
+          <span className={css.quantity}>{stats.views}</span>
         </li>
         <li className={css.listItem}>
           <span className={css.info}>Likes</span>
-          <span className={css.quantity}>{userData.stats.likes}</span>
+          <span className={css.quantity}>{stats.likes}</span>
         </li>
       </ul>
     </div>
   );
-};
+}
