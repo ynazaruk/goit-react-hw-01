1a7c2ec8d1d99113ecf4d98abc97960b4f009c37
ynazaruk <nazaruk.ya@gmail.com>
2024-06-20 19:59:43 +0300

    12

--- src/components/Transaction/Transaction.jsx
+++ src/components/Transaction/Transaction.jsx
@@ -1,11 +1,12 @@
+import css from "../Transaction/Transaction.module.css";
 export default function Transaction({
   transaction: { type, amount, currency },
 }) {
   return (
-    <tr>
-      <td>{type}</td>
-      <td>{amount}</td>
-      <td>{currency}</td>
+    <tr className={css.tr}>
+      <td className={css.td}>{type}</td>
+      <td className={css.td}>{amount}</td>
+      <td className={css.td}>{currency}</td>
     </tr>
   );
 }
